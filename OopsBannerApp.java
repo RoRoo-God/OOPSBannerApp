90ae75b1abd31bdd24c7e34589c825bc1320fdf9
Pushkar Tyagi <pt2645@srmist.edu.in>
2026-03-11 13:34:39 +0530

    Update OopsBannerApp2.java

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,5 +1,13 @@
-public class OopsBannerApp{
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
+public class OOPSBannerApp3 {
+public static void main(String[] args){
+	System.out.println(String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "));
+	System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", "  **     "));
+	System.out.println(String.join(" ", "**     **", "**     **", "**     **", " **      "));
+	System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", "  **     "));
+	System.out.println(String.join(" ", "**     **", "**     **", "******   ", "    ***  "));
+	System.out.println(String.join(" ", "**     **", "**     **", "**       ", "      ** "));
+	System.out.println(String.join(" ", "**     **", "**     **", "**       ", "       **"));
+	System.out.println(String.join(" ", " **   ** ", " **   ** ", "**       ", "      ** "));
+	System.out.println(String.join(" ", "   ***   ",  "  ***   ", "**       ", "  *****  "));
+}
 }
